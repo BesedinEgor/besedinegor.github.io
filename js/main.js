ca6c9bf2db63e5ca703433de62c34b689094a553
Egor Besedin <Besedinegor@mail.ru>
2022-03-06 16:09:50 +0300

    fixed adaptive

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let burger = document.querySelector('.header__burger');
 let menu = document.querySelector('.left__main');
 let hidden = document.querySelector('.wrapper');
 
-burger.onclick = function() {
+burger.onclick = function () {
     menu.classList.toggle('closed');
     hidden.classList.toggle('hidden');
 }
